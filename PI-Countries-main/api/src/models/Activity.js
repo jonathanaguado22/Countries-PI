89324099779a9a29619ed8bc420b4f752b0e91e7
Cesar Aguado <cesaraguado123@gmail.com>
2023-06-04 10:42:38 -0500

    correct models

--- PI-Countries-main/api/src/models/Activity.js
+++ PI-Countries-main/api/src/models/Activity.js
@@ -1,15 +1,15 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('activity', {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: UUIDV4
       },
-    nameActivity: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
       
@@ -24,10 +24,11 @@ module.exports = (sequelize) => {
         },
         },
       duration: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
       },
       season: {
-        type: DataTypes.STRING,
+        type: DataTypes.ENUM('Summer', 'Autum', 'Winter', 'Spring'),
+        allowNull: false,
       }
   },
   { 
--- PI-Countries-main/api/src/models/ActivityByCountry.js
+++ PI-Countries-main/api/src/models/ActivityByCountry.js
@@ -4,63 +4,7 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('activityByCountry', {
-    id: {
-        type: DataTypes.UUID,
-      primaryKey: true,
-      allowNull: false,
-      defaultValue: DataTypes.UUIDV4,
-      
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      
-    },
-    flag: {
-      type: DataTypes.STRING,
-      
-    },
-    continent: {
-      type: DataTypes.STRING,
-      
-    },
-    capital: {
-      type: DataTypes.STRING,
-      
-
-    },
-    subregion: {
-      type: DataTypes.STRING,
-      
-    },
-    area: {
-      type: DataTypes.STRING,
-      
-    },
-    population: {
-      type: DataTypes.STRING,
-      
-    },
-    nameActivity: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      
-    },
-    difficulty: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          isIn: {
-            args: [[1, 2, 3, 4, 5]]
-          },
-        },
-        },
-      duration: {
-        type: DataTypes.INTEGER,
-      },
-      season: {
-        type: DataTypes.STRING,
-      }
+   
   },
   { 
     timestamps: false
